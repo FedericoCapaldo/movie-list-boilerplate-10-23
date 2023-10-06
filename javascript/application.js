5774499be65ae53565df939f5894f9c093cbd6c0
Federico <federico.capaldo@hotmail.com>
2023-10-06 21:35:52 +0100

    installed Vue.js (and removed all moustache code and template)

--- javascript/application.js
+++ javascript/application.js
@@ -1,14 +1,1 @@
-import Mustache from "mustachejs";
-
-const results = document.querySelector("#results");
-const template = document.querySelector("#movieCardTemplate");
-
-fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
-  .then(response => response.json())
-  .then((data) => {
-
-    const movieData = { "movies": data.Search }
-    const output = Mustache.render(template.innerHTML, movieData);
-    results.innerHTML = output;
-
-  })
+import { createApp } from "vue";
