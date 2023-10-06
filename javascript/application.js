de955a9e44abe40b328eabd026d9ffcd884cfb39
Federico <federico.capaldo@hotmail.com>
2023-10-06 21:02:41 +0100

    actually using html template in javascript to add each movie onto page

--- javascript/application.js
+++ javascript/application.js
@@ -1,21 +1,19 @@
+const results = document.querySelector("#results");
+const template = document.querySelector("#movieCardTemplate");
 
 fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
   .then(response => response.json())
   .then((data) => {
 
     data.Search.forEach((result) => {
-      const movieCard = `
-        <div class="col-3">
-          <div class="card" style="width: 18rem;">
-          <img src="${result.Poster}" class="card-img-top" alt="${result.Title} poster">
-          <div class="card-body">
-            <h6 class="card-title">${result.Title}</h6>
-            <p class="card-text">${result.Year}</p>
-            <a href="https://www.imdb.com/title/${result.imdbID}"class="btn btn-primary" target="_blank" >Go to IMDB</a>
-          </div>
-          </div>
-        </div>
-      `
+      const clone = template.content.cloneNode(true);
+
+      clone.querySelector("img").src = result.Poster;
+      clone.querySelector("h6").textContent = result.Title;
+      clone.querySelector("p").textContent = result.Year;
+      clone.querySelector("a").href = `https://www.imdb.com/title/${result.imdbID}`;
+
+      results.appendChild(clone);
     })
 
   })
