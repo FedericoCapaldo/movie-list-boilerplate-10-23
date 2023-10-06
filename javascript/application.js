dbed78649f7aad085cbb96b3af24ad4863010888
Federico <federico.capaldo@hotmail.com>
2023-10-06 20:18:22 +0100

    removed static movies, and making omdbapi API request to console log harry potter movies

--- javascript/application.js
+++ javascript/application.js
@@ -1,1 +1,6 @@
-console.log("hello from application.js")
+fetch("http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7")
+  .then(response => response.json())
+  .then((data) => {
+    console.log(data)
+    // do something with the data
+  })
